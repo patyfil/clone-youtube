a71e5ba355b557f5ba9f0b670a609122d4df4a07
patyfil <patyfil@gmail.com>
2023-09-06 22:23:43 -0300

    :tada:container tela com menu sand

--- js/script.js
+++ js/script.js
@@ -1,19 +1,33 @@
 var sanduiche = document.querySelector(".menu-sanduiche");
-var sidebar = document.querySelector(".sidebar");
-var video = document.querySelector (".video-container");
-var filtros = document.querySelector(".filtros");
-var inscricoes = document.querySelector(".inscricoes");
-var explorar = document.querySelector(".explorar");
-var linha = document.querySelector(".linha");
-
-
+var sidebars = document.querySelectorAll(".sidebar");
+var videoAreas = document.querySelectorAll(".video-container");
+var filtros = document.querySelectorAll(".filtros");
+var inscricoes = document.querySelectorAll(".inscricoes");
+var explorar = document.querySelectorAll(".explorar");
+var linhas = document.querySelectorAll(".linha");
+var videos = document.querySelectorAll(".video");
 
 sanduiche.onclick = function() {
-  sidebar.classList.toggle("small-sidebar");
-  video.classList.toggle("aumenta-video-container");
-  filtros.classList.toggle("small-filtros");
-  inscricoes.classList.toggle("esconde-inscricoes");
-  explorar.classList.toggle("esconde-explorar");
-  linha.classList.toggle("esconde-linha");
-
+  // Itera sobre todos os elementos encontrados com a classe e aplica as alterações
+  sidebars.forEach(function(sidebar) {
+    sidebar.classList.toggle("small-sidebar");
+  });
+  videoAreas.forEach(function(videoArea) {
+    videoArea.classList.toggle("aumenta-video-container");
+  });
+  filtros.forEach(function(filtro) {
+    filtro.classList.toggle("small-filtros");
+  });
+  inscricoes.forEach(function(inscricao) {
+    inscricao.classList.toggle("esconde-inscricoes");
+  });
+  explorar.forEach(function(itemExplorar) {
+    itemExplorar.classList.toggle("esconde-explorar");
+  });
+  linhas.forEach(function(linha) {
+    linha.classList.toggle("esconde-linha");
+  });
+  videos.forEach(function(video) {
+    video.classList.toggle("aumenta-video");
+  });
 }
